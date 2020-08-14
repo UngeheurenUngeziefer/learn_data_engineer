56a0504ec130879365fb82b4f6d1c6e8b0c58e21
Jauhien Leaniuk <59206490+UngeheurenUngeziefer@users.noreply.github.com>
2020-08-14 16:44:31 +0300

    LeetCode added

---
+++ LeetCode/Two Sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i, j]
