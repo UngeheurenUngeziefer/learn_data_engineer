b7e2d82e7659d7f9366839f0654f3f6bc5b882a9
Jauhien Leaniuk <59206490+UngeheurenUngeziefer@users.noreply.github.com>
2020-09-14 17:11:48 +0300

    tests added

---
+++ Tests/test_name_function.py
@@ -0,0 +1,16 @@
+import unittest
+from name_function import get_formatted_name
+
+class NamesTestCase(unittest.TestCase):
+	"""Тесты для 'name_function.py'."""
+	def test_first_last_name(self):
+		"""Работают ли такие имена, как 'Janis Joplin'?"""
+		formatted_name = get_formatted_name('janis', 'joplin')
+		self.assertEqual(formatted_name, 'Janis Joplin')
+	
+	def test_first_last_middle_name(self):
+		"""Работают ли такие имена, как 'Wolfgang Amadeus Mozart'?"""
+		formatted_name = get_formatted_name('wolfgang', 'mozart', 'amadeus')
+		self.assertEqual(formatted_name, 'Wolfgang Amadeus Mozart')
+
+unittest.main()
