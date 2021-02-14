a43536d1dc6a45985a87ed50308f9dedb74d5abb
Jauhien Leaniuk <59206490+UngeheurenUngeziefer@users.noreply.github.com>
2021-02-14 17:23:08 +0300

    files initialized

--- TPCH_Data_project/to_SQL_transformation.py
+++ TPCH_Data_project/to_SQL_transformation.py
@@ -1,1 +1,24 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import pyodbc
 
+# original csv to pandas dataframe
+region = pd.read_csv('tpch_data/h_region.csv', delimiter=',')	
+
+# pandas dataframe to sql database
+engine = create_engine('mssql://sewer:@localhost/h_region', echo=False)
+
+region.to_sql(name='h_region', con=engine, if_exists='replace')
+
+
+
+
+
+
+# server = 'DESKTOP-SAACP1U/MSSQLSERVER01' 
+# database = 'TestSQL' 
+# username = 'DESKTOP-SAACP1U/sewer' 
+# password = '' 
+# PORT='1433'
+# con = pyodbc.connect(r'DRIVER={SQL Server};SERVER='+server+'\\;DATABASE='+database+';UID='+username+';PWD='+ password+';PORT='+PORT)
+# engine.execute("SELECT * FROM h_region").fetchall()
